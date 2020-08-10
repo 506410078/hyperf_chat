37d467d8f8ad9888d5d4e1d3f4994ea8a334655a
506410078 <506410078@qq.com>
2020-08-10 17:41:09 +0800

    first

---
+++ config/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+use Hyperf\HttpServer\Router\Router;
+
+//Router::get('/get', 'App\Controller\IndexController::index');
+//Router::get('/get', 'App\Controller\IndexController@get');
+//Router::get('/get', [\App\Controller\IndexController::class, 'get']);
+
+//Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
+Router::addRoute(['GET', 'POST', 'HEAD'], '/home', 'App\Controller\IndexController::index');
+
